BluezTestBot/bluez

commit bd0808bf01eb4debe450dcd36ab6cddfd3b57779

client: Add advertise.secondary command
This adds advertose.secondary command which can be used to set a
secondary channel to advertise.

client/advertising.c
@@ -64,6 +64,7 @@ static struct ad {
 	bool registered;
 	char *type;
 	char *local_name;
+	char *secondary;
 	uint16_t local_appearance;
 	uint16_t duration;
 	uint16_t timeout;
@@ -441,6 +442,20 @@ static gboolean get_discoverable_timeout(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
+static gboolean secondary_exits(const GDBusPropertyTable *property, void *data)
+{
+	return ad.secondary ? TRUE : FALSE;
+}
+
+static gboolean get_secondary(const GDBusPropertyTable *property,
+					DBusMessageIter *iter, void *user_data)
+{
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING,
+							&ad.secondary);
+
+	return TRUE;
+}
+
 static const GDBusPropertyTable ad_props[] = {
 	{ "Type", "s", get_type },
 	{ "ServiceUUIDs", "as", get_uuids, NULL, uuids_exists },
@@ -456,6 +471,7 @@ static const GDBusPropertyTable ad_props[] = {
 	{ "Appearance", "q", get_appearance, NULL, appearance_exits },
 	{ "Duration", "q", get_duration, NULL, duration_exits },
 	{ "Timeout", "q", get_timeout, NULL, timeout_exits },
+	{ "SecondaryChannel", "s", get_secondary, NULL, secondary_exits },
 	{ }
 };
 
@@ -916,6 +932,33 @@ void ad_advertise_timeout(DBusConnection *conn, long int *value)
 	ad.timeout = *value;
 
 	g_dbus_emit_property_changed(conn, AD_PATH, AD_IFACE, "Timeout");
+
+	return bt_shell_noninteractive_quit(EXIT_SUCCESS);
+}
+
+void ad_advertise_secondary(DBusConnection *conn, const char *value)
+{
+	if (!value) {
+		if (ad.secondary)
+			 bt_shell_printf("Secondary Channel: %s\n",
+							ad.secondary);
+		return bt_shell_noninteractive_quit(EXIT_SUCCESS);
+	}
+
+	if (ad.secondary && !strcmp(value, ad.secondary))
+		return bt_shell_noninteractive_quit(EXIT_SUCCESS);
+
+	free(ad.secondary);
+
+	if (value[0] == '\0') {
+		ad.secondary = NULL;
+		return bt_shell_noninteractive_quit(EXIT_SUCCESS);
+	}
+
+	ad.secondary = strdup(value);
+
+	g_dbus_emit_property_changed(conn, AD_PATH, AD_IFACE,
+							"SecondaryChannel");
 
 	return bt_shell_noninteractive_quit(EXIT_SUCCESS);
 }

client/advertising.h
@@ -41,3 +41,4 @@ void ad_advertise_data(DBusConnection *conn, int argc, char *argv[]);
 void ad_disable_data(DBusConnection *conn);
 void ad_advertise_discoverable(DBusConnection *conn, dbus_bool_t *value);
 void ad_advertise_discoverable_timeout(DBusConnection *conn, long int *value);
+void ad_advertise_secondary(DBusConnection *conn, const char *value);

client/main.c
@@ -2479,6 +2479,16 @@ static void cmd_advertise_timeout(int argc, char *argv[])
 	ad_advertise_timeout(dbus_conn, &value);
 }
 
+static void cmd_advertise_secondary(int argc, char *argv[])
+{
+	if (argc < 2) {
+		ad_advertise_secondary(dbus_conn, NULL);
+		return;
+	}
+
+	ad_advertise_secondary(dbus_conn, argv[1]);
+}
+
 static void ad_clear_uuids(void)
 {
 	ad_disable_uuids(dbus_conn);
@@ -2530,6 +2540,13 @@ static void ad_clear_timeout(void)
 	ad_advertise_timeout(dbus_conn, &value);
 }
 
+static void ad_clear_secondary(void)
+{
+	const char *value = "";
+
+	ad_advertise_secondary(dbus_conn, value);
+}
+
 static const struct clear_entry ad_clear[] = {
 	{ "uuids",		ad_clear_uuids },
 	{ "service",		ad_clear_service },
@@ -2540,6 +2557,7 @@ static const struct clear_entry ad_clear[] = {
 	{ "appearance",		ad_clear_appearance },
 	{ "duration",		ad_clear_duration },
 	{ "timeout",		ad_clear_timeout },
+	{ "secondary",		ad_clear_secondary },
 	{}
 };
 
@@ -2583,6 +2601,8 @@ static const struct bt_shell_menu advertise_menu = {
 			"Set/Get advertise duration" },
 	{ "timeout", "[seconds]", cmd_advertise_timeout,
 			"Set/Get advertise timeout" },
+	{ "secondary", "[1M/2M/Coded]", cmd_advertise_secondary,
+			"Set/Get advertise secondary channel" },
 	{ "clear", "[uuids/service/manufacturer/config-name...]", cmd_ad_clear,
 			"Clear advertise config" },
 	{ } },

commit 5ac7ec0c3126ad4619e9e70afeeac66acf0f5ce6

client/advertising: Fix typos: *_exits --> *_exists
Fixes: bd0808bf01eb ("client: Add advertise.secondary command")
Fixes: 4e7780d0eb94 ("client/advertising: Add support for setting min/max intervals")
Fixes: 068e0ba214b9 ("client: Add set-advertise-{duration, timeout}")
Fixes: 5c3a39e642d5 ("client: Enable set-advertise-appearance to set Appearance")
Fixes: 177eccc14523 ("client: Enable set-advertise-name to set LocalName")
Fixes: 806276fe54fa ("client: Add advertise.discoverable-timeout command")
Signed-off-by: Christian Eggers <ceggers@arri.de>

client/advertising.c
@@ -329,7 +329,7 @@ static gboolean get_includes(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
-static gboolean local_name_exits(const GDBusPropertyTable *property, void *data)
+static gboolean local_name_exists(const GDBusPropertyTable *property, void *data)
 {
 	return ad.local_name ? TRUE : FALSE;
 }
@@ -342,7 +342,7 @@ static gboolean get_local_name(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
-static gboolean appearance_exits(const GDBusPropertyTable *property, void *data)
+static gboolean appearance_exists(const GDBusPropertyTable *property, void *data)
 {
 	return ad.local_appearance != UINT16_MAX ? TRUE : FALSE;
 }
@@ -356,7 +356,7 @@ static gboolean get_appearance(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
-static gboolean duration_exits(const GDBusPropertyTable *property, void *data)
+static gboolean duration_exists(const GDBusPropertyTable *property, void *data)
 {
 	return ad.duration;
 }
@@ -369,7 +369,7 @@ static gboolean get_duration(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
-static gboolean timeout_exits(const GDBusPropertyTable *property, void *data)
+static gboolean timeout_exists(const GDBusPropertyTable *property, void *data)
 {
 	return ad.timeout;
 }
@@ -420,7 +420,7 @@ static gboolean get_discoverable(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
-static gboolean discoverable_timeout_exits(const GDBusPropertyTable *property,
+static gboolean discoverable_timeout_exists(const GDBusPropertyTable *property,
 							void *data)
 {
 	return ad.discoverable_to;
@@ -435,7 +435,7 @@ static gboolean get_discoverable_timeout(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
-static gboolean secondary_exits(const GDBusPropertyTable *property, void *data)
+static gboolean secondary_exists(const GDBusPropertyTable *property, void *data)
 {
 	return ad.secondary ? TRUE : FALSE;
 }
@@ -449,7 +449,7 @@ static gboolean get_secondary(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
-static gboolean min_interval_exits(const GDBusPropertyTable *property,
+static gboolean min_interval_exists(const GDBusPropertyTable *property,
 							void *data)
 {
 	return ad.min_interval;
@@ -464,7 +464,7 @@ static gboolean get_min_interval(const GDBusPropertyTable *property,
 	return TRUE;
 }
 
-static gboolean max_interval_exits(const GDBusPropertyTable *property,
+static gboolean max_interval_exists(const GDBusPropertyTable *property,
 							void *data)
 {
 	return ad.max_interval;
@@ -488,15 +488,15 @@ static const GDBusPropertyTable ad_props[] = {
 	{ "Data", "a{yv}", get_data, NULL, data_exists },
 	{ "Discoverable", "b", get_discoverable, NULL, discoverable_exists },
 	{ "DiscoverableTimeout", "q", get_discoverable_timeout, NULL,
-						discoverable_timeout_exits },
+						discoverable_timeout_exists },
 	{ "Includes", "as", get_includes, NULL, includes_exists },
-	{ "LocalName", "s", get_local_name, NULL, local_name_exits },
-	{ "Appearance", "q", get_appearance, NULL, appearance_exits },
-	{ "Duration", "q", get_duration, NULL, duration_exits },
-	{ "Timeout", "q", get_timeout, NULL, timeout_exits },
-	{ "MinInterval", "u", get_min_interval, NULL, min_interval_exits },
-	{ "MaxInterval", "u", get_max_interval, NULL, max_interval_exits },
-	{ "SecondaryChannel", "s", get_secondary, NULL, secondary_exits },
+	{ "LocalName", "s", get_local_name, NULL, local_name_exists },
+	{ "Appearance", "q", get_appearance, NULL, appearance_exists },
+	{ "Duration", "q", get_duration, NULL, duration_exists },
+	{ "Timeout", "q", get_timeout, NULL, timeout_exists },
+	{ "MinInterval", "u", get_min_interval, NULL, min_interval_exists },
+	{ "MaxInterval", "u", get_max_interval, NULL, max_interval_exists },
+	{ "SecondaryChannel", "s", get_secondary, NULL, secondary_exists },
 	{ }
 };
 